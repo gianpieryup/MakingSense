110f234f573a18d12a10411a2d8a3e0407c99b35
gianpieryup <gyupanquisalvatierra@gmail.com>
2021-08-27 14:48:17 -0700

    creacion del proyecto

---
+++ Cars/Cars/CarCRUD.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Cars
+{
+    class CarCRUD
+    {
+        public Car Create(Car car)
+        {
+            string jsonString = JsonSerializer.Serialize(car);
+            Console.WriteLine(jsonString);
+            // Asignacion de campos de la BD : ID, Fecha_creacion ,etc
+            return car;
+        }
+
+        //public Car Get(int id)
+        //{
+        //    // Code here
+        //}
+
+        //public Car Update(Car car)
+        //{
+        //    // Code here
+        //}
+        public void Delete(int id)
+        {
+            // Code here
+        }
+    }
+}
+
+
---
+++ Cars/Cars/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Cars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Car carrito = new Car("Volvo", "K47", 4, "ROJO", true);
+            Console.WriteLine("Carrito = " + carrito.Marca + " " + carrito.Modelo);
+
+        }
+    }
+
+    class Car
+    {
+        // Valores de persistencia que me los dara la BD
+        public int id { get; }
+        public DateTime fecha_creacion { get; }
+
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int Cant_puertas { get; set; }
+        public string Color { get; set; }
+        public bool Es_manual;
+
+        public Car(string marca, string modelo, int cant_puertas, string color, bool es_manual)
+        {
+            Marca = marca;
+            Modelo = modelo;
+            Cant_puertas = cant_puertas;
+            Color = color;
+            Es_manual = es_manual;
+        }
+    }
+}
