e86f150ff14d75381a4baa9ea3b8e225a228ff65
gianpieryup <gyupanquisalvatierra@gmail.com>
2021-09-04 20:26:27 -0700

    class Car create

---
+++ Cars/Cars/Car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public enum kind { Manual, Automatico };
+
+    class Car
+    {
+        // Valores de persistencia que me los dara la BD
+        public int id { get; set; }
+        public string fecha_creacion { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int Puertas { get; set; }
+        public string Color { get; set; }
+        public kind KindCar { get; set; }
+
+        public Car(string marca, string modelo, int puertas, string color, kind kindCar)
+        {
+            Marca = marca;
+            Modelo = modelo;
+            Puertas = puertas;
+            Color = color;
+            KindCar = kindCar;
+        }
+
+        public string printear()
+        {
+            string name = string.Format("Carro: {0} {1} {2}", Marca, Modelo, Color);
+            return name;
+        }
+    }
+}
--- Cars/Cars/Program.cs
+++ Cars/Cars/Program.cs
@@ -12,11 +12,11 @@ namespace Cars
             CarCRUD bd = new CarCRUD();
 
             Console.WriteLine("\nAGREGANDO nuevo carro");
-            Car carNew = new Car("Ford", "K21", 4, "ROJO",kind.MA); 
+            Car carNew = new Car("Ford", "K21", 4, "ROJO",kind.Manual); 
             Console.WriteLine(carNew.printear());
             bd.Create(carNew);
             
-            
+            /*
             Console.WriteLine("\nGET ID = 0");
             Car carGet = bd.get(2); // Verificar que exista TODO : atraparlo con un exepcion
             Console.WriteLine(carGet.printear());
@@ -25,36 +25,11 @@ namespace Cars
             Car carUpdate = new Car("Chevrolet", "U90", 2, "AZUL",kind.LU);
             carUpdate.id = 1;
             bd.Update(carUpdate);
-            /*
+            
             Console.WriteLine("\nDELETEAR el carro con id = 0"); // Verificar que exista TODO : atraparlo con un exepcion
             bd.Delete(4);*/
         }
     }
     
-    public enum kind { LU, MA, MI };
-    class Car
-    {
-        // Valores de persistencia que me los dara la BD
-        public int id { get; set; }
-        public string fecha_creacion { get; set; }
-        public string Marca { get; set; }
-        public string Modelo { get; set; }
-        public int Puertas { get; set; }
-        public string Color { get; set; }
-        public kind KindCar { get; set; }
-
-        public Car(string marca, string modelo, int puertas, string color, kind kindCar)
-        {
-            Marca = marca;
-            Modelo = modelo;
-            Puertas = puertas;
-            Color = color;
-            KindCar = kindCar;
-        }
-
-        public string printear(){
-            string name = string.Format("Carro: {0} {1} {2}", Marca, Modelo, Color);
-            return name;
-        }
-    }
+    
 }
